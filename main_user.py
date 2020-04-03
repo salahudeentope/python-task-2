40213df0a817e39b89d2c2cbddb2d8b7c0f0fd22
Tope Salahudeen <salahudeentope@gmail.com>
2020-04-03 14:04:15 +0100

    added comments to the code, and added a selection string in the 'invalid selection' prompts

--- main_user.py
+++ main_user.py
@@ -4,12 +4,17 @@ import re
 
 
 def password_generator():
+    """
+    a function to collect user inputs i.e email, first_name and last_name.
+    It then automatically generates a password from the inputs.
+    """
     email = input("Please enter your email: ")
-    while not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email):
+    while not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email): # regex to validate email
         email = input('please enter a valid email address: ')
     first = input("Please enter your first name: ")
     last = input('Please enter your last name: ')
 
+    # the code below is used to generate a random 5-character string from ascii letters
     letters = string.ascii_letters
     random_string = "".join(random.sample(letters, 5))
     password = first[0:2] + last[-2:] + random_string
@@ -17,6 +22,14 @@ def password_generator():
 
 
 def user_details_container():
+    """
+    Function to interactively collect and store users details in a container continously
+    until terminated when the last user is inputed.
+    Users names are stored in a Title Case format.
+
+    accepted inputs from interactive questions are ['yes', 'y', 'no', 'n'].
+
+    """
     users_container = dict()
     new_user = "yes"
 
@@ -26,15 +39,15 @@ def user_details_container():
         print(password)
         choice = input("Do you like this password? [y/n]: ")
         while not choice.lower().replace(' ', '') in ['yes', 'y', 'no', 'n']:
-            choice = input("please input a valid selection: ")
+            choice = input("please input a valid selection [y/n]: ")
         if choice.lower().replace(' ', '') in ['yes', 'y']:  # the .replace to take care of accidental spaces
             print('congratulation your account has been opened successfully')
             users_container[email]['password'] = password
 
-            new_user = input("Do you have a new user to input?[y/n]: ")
+            new_user = input("Do you have a new user to input? [y/n]: ")
 
             while not new_user.lower().replace(' ', '') in ['yes', 'y', 'no', 'n']:
-                new_user = input("please input a valid selection: ")
+                new_user = input("please input a valid selection [y/n]: ")
             new_user = new_user.lower().replace(' ', '')
             continue
         elif choice.lower().replace(' ', '') in ['no', 'n']:
@@ -47,7 +60,7 @@ def user_details_container():
 
             new_user = input("Do you have a new user to input?[y/n]: ")
             while not new_user.lower().replace(' ', '') in ['yes', 'y', 'no', 'n']:
-                new_user = input("please input a valid selection: ")
+                new_user = input("please input a valid selection [y/n]: ")
             new_user = new_user.lower().replace(' ', '')
             continue
 
@@ -55,6 +68,11 @@ def user_details_container():
 
 
 def users():
+    """
+    The function prints the users details after the last user is inputted.
+    It prints the users details nicely without mixing up any of the values.
+    """
+
     users_dict = user_details_container()
     print()
     for i, user in enumerate(users_dict.values()):
